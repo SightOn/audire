909692b5dd1c4b3bc3fc646eef5c7013e06c840f
inatani <inai17ibar@gmail.com>
2018-08-04 22:02:37 +0900

    Add Xcode project.
    Xcode9.4, Swift4.1

---
+++ audire/audire/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  audire
+//
+//  Created by Soichiro Inatani on 2018/08/04.
+//  Copyright © 2018年 SightOn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
